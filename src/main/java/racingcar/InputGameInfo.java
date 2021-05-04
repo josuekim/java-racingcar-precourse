6339fff93a5969a35a392e75a39af0f021248d26
josuekim <lovehot320@gmail.com>
2021-05-04 20:15:38 +0900

    style : change output text
    Input 메세지 수정
    줄바꿈

--- src/main/java/racingcar/InputGameInfo.java
+++ src/main/java/racingcar/InputGameInfo.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputGameInfo {
-    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분";
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUND_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String INPUT_ERROR_MESSAGE = "숫자를 입력해 주세요";
     private final Scanner scan;
--- src/main/java/racingcar/OutputGameInfo.java
+++ src/main/java/racingcar/OutputGameInfo.java
@@ -9,6 +9,7 @@ public class OutputGameInfo {
     public static void printWinners(Cars cars) {
         CarFinalResult carFinalResult = new CarFinalResult(cars.getCars());
         String result = String.format(WINNER_ANNOUNCE_MESSAGE, joinWinnerString(carFinalResult.getWinners()));
+        System.out.println();
         System.out.println(result);
     }
 
@@ -21,6 +22,7 @@ public class OutputGameInfo {
     }
 
     public static void printProgress(Cars cars) {
+        System.out.println();
         for (Car car : cars.getCars()) {
             CarResult carResult = new CarResult(car);
             System.out.println(carResult.printMovePosition());

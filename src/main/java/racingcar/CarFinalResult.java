b5084713ae9ae30c0673554e64735215cb736721
josuekim <lovehot320@gmail.com>
2021-05-04 19:16:08 +0900

    feat(CarFinalResult) : add CarFinalResult class
    경주 우승 결과 class
    - 우승자 찾기 메소드
    - 우승자 리스트

---
+++ src/main/java/racingcar/CarFinalResult.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CarFinalResult {
+    private final List<Car> cars;
+    private int maxPosition;
+    private List<String> winners;
+
+    public CarFinalResult(List<Car> cars) {
+        this.cars = cars;
+        this.winners = new ArrayList<>();
+    }
+
+    public List<String> getWinners() {
+        sortCarRanking();
+        return winners;
+    }
+
+    private void sortCarRanking() {
+        Collections.sort(cars, (s1, s2) -> s1.getPosition() - s2.getPosition());
+        maxPosition = cars.get(0).getPosition();
+        setWinners();
+    }
+
+    private void setWinners() {
+        for (Car car : cars) {
+            setWinner(car);
+        }
+    }
+
+    private void setWinner(Car car) {
+        if (car.getPosition() == maxPosition) {
+            winners.add(car.getCar());
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarFinalResultTest.java
@@ -0,0 +1,38 @@
+package racingcar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarFinalResultTest {
+    private Cars cars;
+
+    @BeforeEach
+    public void setUpCar() {
+        cars = new Cars("pobi,konan,josue");
+    }
+
+    @Test
+    @DisplayName("우승자 찾기 테스트")
+    public void winnerTest() {
+        Car car1 = cars.getCars().get(0);
+        car1.play(4);
+        car1.play(1);
+        Car car2 = cars.getCars().get(1);
+        car2.play(4);
+        car1.play(6);
+        Car car3 = cars.getCars().get(2);
+        car3.play(4);
+        car1.play(6);
+
+        CarFinalResult carFinalResult = new CarFinalResult(cars.getCars());
+        List<String> winners = carFinalResult.getWinners();
+        assertThat(winners.get(0)).isEqualTo("konan");
+        assertThat(winners.get(1)).isEqualTo("josue");
+    }
+}
